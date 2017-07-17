c2d1079d9e4a8ef49b0f05263597f3a4ea8cd1c7
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 20:24:54 +0300

    Add files via upload

---
+++ %20 indirim hesaplama/checkbox kullanımı 20 indirim ölçme/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double tutar, toplam;
+                tutar = Convert.ToInt32(textBox1.Text);
+                if (checkBox1.Checked == true)
+                {
+                    toplam = tutar - (tutar * 0.2);
+                }
+                else
+                {
+                    toplam = tutar;
+                }
+                label2.Text = toplam.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("lütfen boş alanı doldurunuz","HATA!!",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+            }
+           }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+       
+
+
+    }
+}
